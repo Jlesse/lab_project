0f5538e090a4e31cef248c609bbd808ee8ba4672
Troy Kuziel <leizuk777@gmail.com>
2015-12-08 08:09:08 -0600

    Add changes to routes and some forms

--- client_project/config/routes.rb
+++ client_project/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root 'experiments#index'
 resources :users
+
 resources :experiments
+
 resources :comments
+
 resources :observations
+
 resources :proposals
+
 end
