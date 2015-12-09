52e186ad7dd796c185d7805f72ebf3db483acca7
Lily-Therese Walla <voilawalla411@gmail.com>
2015-12-09 11:14:26 -0600

    resolve accidental edit to app

--- client_project/app/models/lab_task.rb
+++ client_project/app/models/lab_task.rb
@@ -1,4 +1,26 @@
 class LabTask < ActiveRecord::Base
   belongs_to :log
   belongs_to :procedure
+
+  def self.completed_today
+    where("lab_tasks.created_at >= ?", Date.today)
+  end
+
+  def self.completed_by(user)
+    joins(:log).where("logs.user_id = ?", user.id)
+  end
+
+  # def self.completed_today_by(user)
+  #   completed_today.completed_by(user)
+  # end
 end
+
+#iterate through all of the experiments logs
+#check each of the exeperiments procedures
+  # print procedure
+  # does a given experiment procedure appear in the in the logs.procedures
+  #logs.procedures.include?{|log_pro| log_pro.id == exp_pro.id}
+  # if it does put a check mark
+
+
+
--- client_project/db/seeds.rb
+++ client_project/db/seeds.rb
@@ -14,19 +14,22 @@
                               procedure: Faker::Lorem.paragraph,
                               results: Faker::Lorem.paragraph,
                               conclusion: Faker::Lorem.paragraph,
-                              proposal_id: i,
+                              proposal_id: i + 1,
                               active: true,
                               start_date: Faker::Date.backward(20),
                               end_date: Faker::Date.forward(23))
 
-  proposal = Proposal.create!(experiment_id: i,
+  proposal = Proposal.create!(experiment_id: i + 1,
                             abstract: Faker::Lorem.paragraph,
                             proposed_funding: Faker::Commerce.price
                             )
 
-  procedure = experiment.procedures.create!(text: Faker::Lorem.paragraph)
-  procedure = experiment.procedures.create!(text: Faker::Lorem.paragraph)
-  procedure = experiment.procedures.create!(text: Faker::Lorem.paragraph)
+  procedure = experiment.procedures.create!(text: Faker::Lorem.sentence)
+  procedure = experiment.procedures.create!(text: Faker::Lorem.sentence)
+  procedure = experiment.procedures.create!(text: Faker::Lorem.sentence)
+  procedure = experiment.procedures.create!(text: Faker::Lorem.sentence)
+  procedure = experiment.procedures.create!(text: Faker::Lorem.sentence)
+  procedure = experiment.procedures.create!(text: Faker::Lorem.sentence)
 
 end
 
@@ -42,26 +45,12 @@ end
   user.observations.create!(title: Faker::Lorem.word, text: Faker::Lorem.paragraph,
                             experiment_id: 1 )
 
-  Log.create(user_id: i, observation_id: i, comment: Faker::Lorem.sentence)
+  Log.create(user_id: 1 + i, observation_id: 1 + i, comment: Faker::Lorem.sentence)
+  # Log.create(user_id: 2, observation_id: i, comment: Faker::Lorem.sentence)
 
-  LabTask.create!(log_id: 1, procedure_id: rand(3) + 1)
-
-end
-
-5.times do |i|
-  user = User.create!(first_name: Faker::Name.first_name,
-                    last_name: Faker::Name.last_name,
-                    email: Faker::Internet.free_email,
-                    password: "password",
-                    admin: false)
-
-  user.researcher_experiments.create!(experiment_id: 1 )
-  user.observations.create!(title: Faker::Lorem.word, text: Faker::Lorem.paragraph,
-                            experiment_id: 1 )
-
-  Log.create(user_id: 5 + i, observation_id: 5 + i, comment: Faker::Lorem.sentence)
-
-  LabTask.create!(log_id: 1, procedure_id: rand(4..6))
+  # LabTask.create!(log_id: 1 + i, procedure_id: rand(6) + 1)
+  # LabTask.create!(log_id: 2 + i, procedure_id: rand(6) + 1)
+  LabTask.create!(log_id: 1 + i, procedure_id: rand(6) + 1)
 
 end
 
@@ -72,13 +61,16 @@ end
                     password: "password",
                     admin: false)
 
-  user.researcher_experiments.create!(experiment_id: 1 )
+  user.researcher_experiments.create!(experiment_id: 2 )
   user.observations.create!(title: Faker::Lorem.word, text: Faker::Lorem.paragraph,
-                            experiment_id: 1 )
+                            experiment_id: 2 )
 
-  Log.create(user_id: 10 + i, observation_id: 10 + i, comment: Faker::Lorem.sentence)
+  Log.create(user_id: 6 + i, observation_id: 6 + i, comment: Faker::Lorem.sentence)
+  # Log.create(user_id: 7 + i, observation_id: 7 + i, comment: Faker::Lorem.sentence)
 
-  LabTask.create!(log_id: 1, procedure_id: rand(7..9))
+  LabTask.create!(log_id: 6 + i, procedure_id: rand(7..13))
+  # LabTask.create!(log_id: 7 + i, procedure_id: rand(7..13))
+  # LabTask.create!(log_id: 8 + i, procedure_id: rand(7..13))
 
 end
 
@@ -89,13 +81,17 @@ end
                     password: "password",
                     admin: false)
 
-  user.researcher_experiments.create!(experiment_id: 1 )
+  user.researcher_experiments.create!(experiment_id: 3 )
   user.observations.create!(title: Faker::Lorem.word, text: Faker::Lorem.paragraph,
-                            experiment_id: 1 )
+                            experiment_id: 3 )
 
-  Log.create(user_id: 15 + i, observation_id: 15 + i, comment: Faker::Lorem.sentence)
+  Log.create(user_id: 11 + i, observation_id: 11 + i, comment: Faker::Lorem.sentence)
+  # Log.create(user_id: 12 + i, observation_id: 12 + i, comment: Faker::Lorem.sentence)
 
-  LabTask.create!(log_id: 1, procedure_id: rand(10..12))
+
+  LabTask.create!(log_id: 11 + i, procedure_id: rand(13..19))
+  # LabTask.create!(log_id: 12 + i, procedure_id: rand(13..19))
+  # LabTask.create!(log_id: 13 + i, procedure_id: rand(13..19))
 
 end
 
@@ -106,12 +102,35 @@ end
                     password: "password",
                     admin: false)
 
-  user.researcher_experiments.create!(experiment_id: 1 )
+  user.researcher_experiments.create!(experiment_id: 4 )
   user.observations.create!(title: Faker::Lorem.word, text: Faker::Lorem.paragraph,
-                            experiment_id: 1 )
+                            experiment_id: 4 )
 
-  Log.create(user_id: 20 + i, observation_id: 20 + i, comment: Faker::Lorem.sentence)
+  Log.create(user_id: 16 + i, observation_id: 16 + i, comment: Faker::Lorem.sentence)
+  # Log.create(user_id: 17 + i, observation_id: 17 + i, comment: Faker::Lorem.sentence)
 
-  LabTask.create!(log_id: 1, procedure_id: rand(13..15))
+  LabTask.create!(log_id: 16 + i, procedure_id: rand(19..25))
+  # LabTask.create!(log_id: 17 + i, procedure_id: rand(19..25))
+  # LabTask.create!(log_id: 18 + i, procedure_id: rand(19..25))
+
+end
+
+5.times do |i|
+  user = User.create!(first_name: Faker::Name.first_name,
+                    last_name: Faker::Name.last_name,
+                    email: Faker::Internet.free_email,
+                    password: "password",
+                    admin: false)
+
+  user.researcher_experiments.create!(experiment_id: 5 )
+  user.observations.create!(title: Faker::Lorem.word, text: Faker::Lorem.paragraph,
+                            experiment_id: 5 )
+
+  Log.create(user_id: 21 + i, observation_id: 21 + i, comment: Faker::Lorem.sentence)
+  # Log.create(user_id: 22 + i, observation_id: 22 + i, comment: Faker::Lorem.sentence)
+
+  LabTask.create!(log_id: 21 + i , procedure_id: rand(25..30))
+  # LabTask.create!(log_id: 22 + i , procedure_id: rand(25..30))
+  # LabTask.create!(log_id: 23 + i , procedure_id: rand(25..30))
 
 end
